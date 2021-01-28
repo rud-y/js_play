52c429cbdc84c53b6761e6148f1e06c4a912ef16
rud-y <rudolf.zachar@gmail.com>
2021-01-28 15:48:06 +0000

    .html file and create getJoke function to hold data in cont

--- udDir/fetch.js
+++ udDir/fetch.js
@@ -1,2 +1,17 @@
-const url = "https://api.chucknorris.io/jokes/random";
-const btn = document.querySelector("button");
+window.addEventListener("DOMContentLoaded", () => {
+  const url = "https://api.chucknorris.io/jokes/random";
+  const btn = document.querySelector("button");
+
+  if (btn) {
+    btn.addEventListener("click", getJoke);
+  }
+
+  function getJoke() {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then(function (data) {
+        console.log(data.value);
+        document.querySelector("#jokeDiv").textContent = data.value;
+      });
+  }
+});
